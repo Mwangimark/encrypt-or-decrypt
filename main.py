4846b40c455304596acde025084a7ddd8f88af01
Mwangimark <132432689+Mwangimark@users.noreply.github.com>
2024-02-26 16:00:50 +0000

    implemented while loop

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-if shift > 26:
-  shift = shift % 26
-  shift = shift
 
 def encrypt(text,shift):
   encoded_text = []
@@ -32,24 +25,41 @@ def dencrypt(text,shift):
   encoded_text = []
 
   for i in range (0,len(text)):
-    alphabet_index = alphabet.index(text[i])
-    alphabet_index -= shift
-
-    if alphabet_index < 0 :
-      alphabet_index += 26
-
-    encodedtxt = (alphabet[alphabet_index])
-    encoded_text.append(encodedtxt)
+    if text[i] in alphabet:
+      alphabet_index = alphabet.index(text[i])
+      alphabet_index -= shift
+  
+      if alphabet_index < 0 :
+        alphabet_index += 26
+  
+      encodedtxt = (alphabet[alphabet_index])
+      encoded_text.append(encodedtxt)
+    else :
+      encoded_text.append(text[i])
   encoded_joined_text = ''.join(encoded_text)
   print(f"The decoded message is : {encoded_joined_text}")
 
-# =================/////////================
-if direction == "encode":
-  encrypt(text,shift)
-elif direction == "decode":
-  dencrypt(text,shift)
-else:
-  print("Wrong input")
-# =================//////================
+gaming = True
+
+while gaming:
+    direction = input("Type 'encode' to encrypt, 'decode' to decrypt :").lower()
+
+    text = input("Type your message: ").lower()
+    shift = int(input("Type the shift number: "))
+
+    if shift > 26:
+        shift = shift % 26
+
+    if direction == "encode":
+        encrypt(text, shift)
+    elif direction == "decode":
+        dencrypt(text, shift)
+    else:
+        print("Wrong input")
+
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again == "no":
+        gaming = False
+        print("Goodbye!")
 
 
