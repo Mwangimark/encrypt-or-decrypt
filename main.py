dbb3b7b015c2c04c44597092fd09921f0889af88
Mwangimark <132432689+Mwangimark@users.noreply.github.com>
2024-02-26 16:05:56 +0000

    removed f string

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(art.logo)
 
 def encrypt(text,shift):
   encoded_text = []
@@ -17,7 +20,7 @@ def encrypt(text,shift):
     else:
       encoded_text.append(text[i])
   encoded_joined_text = ''.join(encoded_text)
-  print(f"The coded message is : {encoded_joined_text}")
+  print("The coded message is : "+encoded_joined_text)
 
 
 
@@ -37,7 +40,7 @@ def dencrypt(text,shift):
     else :
       encoded_text.append(text[i])
   encoded_joined_text = ''.join(encoded_text)
-  print(f"The decoded message is : {encoded_joined_text}")
+  print("The decoded message is : "+encoded_joined_text)
 
 gaming = True
 
